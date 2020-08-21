c09204cf693d26e10a72cdd4e383cd991676f85b
Mauro Faccenda <faccenda@gmail.com>
2020-08-21 15:38:22 +0200

    illustrate issue

---
+++ my_module/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def my_function(my_value: int) -> int:
+  return my_value * 2
+
+def my_other_function(my_value: int) -> int:
+  return my_value * 2
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from my_module import main
+
+def test_my_function():
+  assert main.my_function(10) == 20
